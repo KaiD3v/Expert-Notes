a482f3b3d240ba1b1d960ac07281f85d09d851d8
Kaique <kaikricardo99@gmail.com>
2024-02-06 19:15:09 -0300

    fix card

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,6 @@
 export function NoteCard(){
     return(
-        <button className=" outline-none text-left rounded-md bg-slate-700 p-5 space-y-3 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
           <span className="text-sm font-medium text-slate-300">há 2 dias</span>
           <p className=" text-sm leading-6 text-slate-400">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
